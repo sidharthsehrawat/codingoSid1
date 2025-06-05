6482647193fbe2d8a7909026404a5759a82e7d8e
Sidharth Sehrawat <sidharth_sehrawat@epam.com>
2025-06-05 10:19:00 +0530

    d

---
+++ src/main/java/revision/graph/CourseSchedule.java
@@ -0,0 +1,48 @@
+package revision.graph;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class CourseSchedule {
+    public static void main(String[] args) {
+
+    }
+
+    class Solution {
+        public boolean canFinish(int numCourses, int[][] prerequisites) {
+            List<List<Integer>> graph = new ArrayList<>();
+            int[] inDegree = new int[numCourses];
+
+            for (int i = 0; i < numCourses; i++) {
+                graph.add(new ArrayList<>());
+            }
+
+            for (int[] pair : prerequisites) {
+                int course = pair[0];
+                int prereq = pair[1];
+                graph.get(prereq).add(course);
+                inDegree[course]++;
+            }
+
+            Queue<Integer> queue = new LinkedList<>();
+            for (int i = 0; i < numCourses; i++) {
+                if (inDegree[i] == 0) queue.offer(i);
+            }
+
+            int visited = 0;
+
+            while (!queue.isEmpty()) {
+                int curr = queue.poll();
+                visited++;
+                for (int neighbor : graph.get(curr)) {
+                    inDegree[neighbor]--;
+                    if (inDegree[neighbor] == 0) queue.offer(neighbor);
+                }
+            }
+
+            return visited == numCourses;
+        }
+    }
+}
--- src/main/java/z_epam/Test.java
+++ src/main/java/z_epam/Test.java
@@ -1,49 +1,4 @@
 package z_epam;
 
-import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
 public class Test {
-    public static void main(String[] args) {
-        String s = "abcabcbb";
-        int res = lengthOfLongestSubstring(s);
-        //System.out.println("result : " + res);
-
-        String str ="abacbcedf";
-        // a - 2
-        // b - 2
-        // c - 2
-        // d -
-        Map<String, Long> ans = Arrays.stream(str.split("")).collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()));
-        System.out.println("check " + ans);
-        Map.Entry<String, Long> result = ans.entrySet().stream().filter(entry -> entry.getValue() == 1).findAny().get();
-        System.out.println("result " + result.getKey());
-
-       // System.out.println(ans);
-
-    }
-
-    public static int lengthOfLongestSubstring(String str) {
-        int maxLength = 0;
-        int start = 0;
-        int i = 0;
-        Set<Character> set = new HashSet<>();
-        while (i<str.length()){
-            if(!set.contains(str.charAt(i))){
-                set.add(str.charAt(i));
-                i++;
-            }else{
-                set.remove(str.charAt(start));
-                start++;
-            }
-
-            maxLength = Math.max(i - start, maxLength);
-
-        }
-
-        return maxLength;
-    }
-
-
 }
